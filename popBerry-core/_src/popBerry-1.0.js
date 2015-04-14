8d1c025b0ebe86ee4ad8763bc73b5b84f8252c64
李鹏飞 <lipf@ichsy.com>
2015-04-14 18:38:42 +0800

    初始化项目， 添加popBerry的核心js包，已期待后续可以完善该Lib包

---
+++ popBerry-core/_src/popBerry-1.0.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * popBerry javaScript Library v1.0
+ * @Author lipengfei
+ * @Email lipengfei217@163.com
+ * @Date 2015-04-14
+ * 
+ * */
+(function(window,undefined){
+  
+  //Use javaScript's strict mode.Some older browers will ignore this mode
+  
+  "use strict";
+  
+  _popBerry = window.popBerry;
+  
+  
+  
+})(window);
